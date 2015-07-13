4e48f4e00505c8ea839b976785e671c736dd776b
rfung <rfung@zendesk.com>
2015-07-13 12:26:26 -0700

    added some tests added sass compile cleaned up gulp targets fixed some bug in Tooltip, Step
    fixed test

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,55 @@ var run = require('gulp-run');
 var babel = require('babel/register');
 var mocha = require('gulp-mocha');
 var testem = require('gulp-testem');
+var sass = require('gulp-sass');
 
 reportOptions = {
     err: true, // default = true, false means don't write err 
     stderr: true, // default = true, false means don't write stderr 
     stdout: true // default = true, false means don't write stdout 
 }
+
+gulp.task("default", ['js','sass']);
+
+//################ BUILD ####################
+
+gulp.task('watch', ['js:watch', 'sass:watch']);
+
+gulp.task('js', function () {
+  browserify({    
+      entries: './modules/index.js',
+      debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('chariot.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload())
+});
+
+gulp.task('js:watch', function(){
+  return gulp.watch('modules/**/*.js', ['js']);
+});
+
+gulp.task('sass', function () {
+  gulp.src('./stylesheets/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/stylesheets'));
+});
  
+gulp.task('sass:watch', function () {
+  gulp.watch('./stylesheets/**/*.scss', ['sass']);
+});
+
+//################ TEST ####################
+
 gulp.task('connect', ['watch'], function() {
   connect.server({
     root: [__dirname],
     livereload: true
   });
 });
+
 
 gulp.task('test', function() {
   return gulp.src(['test/**/*.js'])
@@ -33,20 +69,11 @@ gulp.task('test', function() {
   }
 );
 
-gulp.task('watch', function(){
-  return gulp.watch('modules/**/*.js', ['default']);
-});
-
-gulp.task("default", function () {
-  browserify({    
-      entries: './modules/index.js',
-      debug: true
-    })
-    .transform(babelify)
-    .bundle()
-    .pipe(source('chariot.js'))
-    .pipe(gulp.dest('./dist'))
-    .pipe(connect.reload())
+gulp.task('testem', ['compile-test'], function () {
+  gulp.src([''])
+    .pipe(testem({
+        configFile: 'testem.yml'
+    }));
 });
 
 gulp.task("compile-test", function () {
@@ -61,16 +88,3 @@ gulp.task("compile-test", function () {
     .pipe(gulp.dest('./dist/test'))
   });
 });
-
-
-gulp.task('testem', ['compile-test'], function () {
-    gulp.src([''])
-        .pipe(testem({
-            configFile: 'testem.yml'
-        }));
-});
-
-
-
-
-
